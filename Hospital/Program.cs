0b62d2a93eacad379d848508df1c46fdb0ddb29f
astrinh0 <jngveloso@gmail.com>
2020-04-23 18:32:22 +0100

    ACTUALIZACAO

--- Hospital/Program.cs
+++ Hospital/Program.cs
@@ -1,4 +1,15 @@
-﻿using System;
+﻿/*
+ * 
+ * <copyright file = "$safeitemrootname$"   Developer: João Veloso </copyright>
+ * <author>jngveloso</author>
+ * <email>jngveloso@gmail.com</email>
+ * <date>$time$</date>
+ * <description></description>
+ * 
+ */
+
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
